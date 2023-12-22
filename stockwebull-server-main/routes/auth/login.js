6729276270e226ef3e6c215d9198af3c0508c19c
AmaraCharles <falsepegasus@gmail.com>
2023-12-22 05:36:56 +0100

    admin user login

--- stockwebull-server-main/routes/auth/login.js
+++ stockwebull-server-main/routes/auth/login.js
@@ -3,6 +3,9 @@ var { compareHashedPassword } = require("../../utils");
 const UsersDatabase = require("../../models/User");
 var router = express.Router();
 
+
+
+
 router.post("/login", async function (request, response) {
   const { email, password } = request.body;
   /**
@@ -27,5 +30,25 @@ router.post("/login", async function (request, response) {
     response.status(404).json({ code: "no user found" });
   }
 });
+
+router.post("/loginadmin", async function (request, response) {
+  const { email} = request.body;
+  /**
+   * step1: check if a user exists with that email
+   * step2: check if the password to the email is correct
+   * step3: if it is correct, return some data
+   */
+
+  // step1
+  const user = await UsersDatabase.findOne({ email: email });
+
+  if (user) {
+    // step2
+   
+      response.status(200).json({ code: "Ok", data: user });
+   
+}});
+
+
 
 module.exports = router;
